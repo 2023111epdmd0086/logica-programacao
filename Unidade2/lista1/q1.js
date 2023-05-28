a7cabeb6d454d0a9c8aabece2fc914ab96760183
Jonas <132786472+2023111epdmd0086@users.noreply.github.com>
2023-05-28 11:31:35 -0300

    Delete Unidade2/lista1 directory

--- Unidade2/lista1/q1.js
+++
@@ -1,9 +0,0 @@
-// Escreva um programa que receba um número inteiro como entrada e imprima o sucessor e antecessor desse número.
-
-let num = parseInt(prompt("Digite o número"));
-// Definindo as variáveis:
-let ant = num - 1;
-let suc = num + 1;
-// Imprimindo no console
-console.log(`O ancecessor de ${num} é: ${ant}`);
-console.log(`O sucessos de ${num} é: ${suc}`);
